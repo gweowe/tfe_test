d67ab11ca1c12a82773f004af140e283e4fe09b5
gweowe <73269658+gweowe@users.noreply.github.com>
2023-05-12 17:42:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "tfe" {
   hostname = "tfe.ddim-test-domain.link"
   token    = ""
 }
-resource "tfe_workspace" "dwwon_workspace" {
+resource "tfe_workspace" "dwwon_gitlab_workspace" {
   name         = "test5"
   organization = "dwwon"
 }
