0d13a87ded1e56c4f4de4515b8ad99e657352bff
gweowe <73269658+gweowe@users.noreply.github.com>
2023-05-12 17:04:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "tfe" {
   hostname = "tfe.ddim-test-domain.link"
-  token    = "ovRfQUSpDJUl7Q.atlasv1.zbtO2SGLOzoYxXdMvSzQWs7rizzWoarghLRCKymIazXXSca4Ts8EPeZzhApS1nwDZdk"
+  token    = ""
 }
 resource "tfe_workspace" "dwwon_workspace" {
   name         = "test2"
